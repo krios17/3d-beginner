ec18100edcf6e807db87d8f3811ca1de895068eb
krios17 <krios@colum.edu>
2021-12-13 08:37:58 -0600

    Audio and Building

---
+++ Crumpled Castles/Assets/Scripts/MapManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MapManager : MonoBehaviour
+{
+	public Character Character;
+	public Pin StartPin;
+	public Text SelectedLevelText;
+
+	private int nextSceneToLoad;
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	private void Start ()
+	{
+		// Pass a ref and default the player Starting Pin
+		Character.Initialise(this, StartPin);
+
+		GameObject.Find("Dice");
+
+		nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+	}
+
+
+	/// <summary>
+	/// This runs once a frame
+	/// </summary>
+	private void Update()
+	{
+		// Only check input when character is stopped
+		if (Character.IsMoving) return;
+
+		
+
+		// First thing to do is try get the player input
+		CheckForInput();
+	}
+
+	
+	/// <summary>
+	/// Check if the player has pressed a button
+	/// </summary>
+	private void CheckForInput()
+	{
+		if (Input.GetKeyUp(KeyCode.UpArrow))
+		{
+			Character.TrySetDirection(Direction.Up);
+		}
+		else if(Input.GetKeyUp(KeyCode.DownArrow))
+		{
+			Character.TrySetDirection(Direction.Down);
+		}
+		else if(Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			Character.TrySetDirection(Direction.Left);
+		}
+		else if(Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			Character.TrySetDirection(Direction.Right);
+		}
+		else if (Input.GetKeyDown(KeyCode.C))
+		{
+			SceneManager.LoadScene(1);
+		}
+	}
+
+	
+	/// <summary>
+	/// Update the GUI text
+	/// </summary>
+	public void UpdateGui()
+	{
+		SelectedLevelText.text = string.Format("Current Level: {0}", Character.CurrentPin.SceneToLoad);
+	}
+}
